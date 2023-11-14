ea08ae05c37f4386372b06fa60ecc462f907bee9
emir215 <108100885+emir215@users.noreply.github.com>
2023-11-14 12:01:27 +0100

    Add files via upload

---
+++ vjezbe.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Broj puta koji zelimo ponoviti poruku
+        int brojPonavljanja = 36;
+
+        // Petlja koja ce ispisati poruku Hello World odredeni broj puta
+        for (int i = 0; i < brojPonavljanja; i++) {
+            System.out.println("Hello World");
+        }
+    }
+}
